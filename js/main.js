d4245c2211a353a22d65e7e4e150107be10e34c8
Mirshod Murodov <77718299+Mirshod-Murodov@users.noreply.github.com>
2021-01-20 14:07:40 +0300

    da
    dasda

---
+++ js/main.js
@@ -0,0 +1,15 @@
+alert("Bildirish nomalarga ruhsat bering!");
+var a = confirm("RAZ RAZ");
+if (a) {
+	alert("ok");
+}
+else {
+	alert("ERROR!")
+}
+var z = prompt("5+10")
+if (z==15) {
+	alert ("SIZ JAVOBNI TOPDINGIZ!")
+}
+else {
+	alert("QAYTADADN URINIB KO'RING")
+}
